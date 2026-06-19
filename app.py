1372e6a984606ba5e71eaf023883cfea1e88d817
Jeremie <jeremieflahaut@gmail.com>
2026-06-19 11:01:42 +0200

    introduce app factory, blueprint and .env-based config

--- app.py
+++ app.py
@@ -1,48 +1,21 @@
 from flask import Flask
-from models import db, Product
-from functools import wraps
 
-app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db.init_app(app)
+from controllers.products import products
+from models import db
 
 
-def error_response(message: str, code: int):
-    return {"error": message}, code
+def create_app(test_config=None):
+    app = Flask(__name__)
 
+    app.config.from_mapping(SQLALCHEMY_DATABASE_URI="sqlite:///database.db")
 
-def try_except(func):
-    """
-    Produit automatiquement une erreur 500 si une exception est levée
-    """
+    if test_config is None:
+        app.config.from_prefixed_env()
+    else:
+        app.config.from_mapping(test_config)
 
-    @wraps(func)
-    def wrapper(**kwargs):
-        try:
-            return func(**kwargs)
-        except Exception as e:
-            return error_response(str(e), 500)
+    db.init_app(app)
 
-    return wrapper
+    app.register_blueprint(products)
 
-
-@app.route("/api/produits", methods=["GET"])
-@try_except
-def products_index():
-    products_db = Product.query.all()
-
-    return [p.to_dict() for p in products_db]
-
-
-@app.route("/api/produits/<int:id>", methods=["GET"])
-@try_except
-def product_show(id: int):
-    product = Product.query.get(id)
-
-    if product is None:
-        return error_response("Product not found", 404)
-
-    return product.to_dict()
+    return app
